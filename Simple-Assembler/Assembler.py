f7750e563b3046cd33e58bf0394392642aa83c28
atharva20038 <atharva20038@iiitd.ac.in>
2021-08-09 17:25:28 +0530

    input_corrections

--- Simple-Assembler/Assembler.py
+++ Simple-Assembler/Assembler.py
@@ -15,9 +15,17 @@ while line_count < 256:
     line = input().strip()
     if line == '':
         continue
+
+    if line == 'hlt' :
+        temp = input().strip()
+        assert  temp=='', "Cant add commands after Halt"
+        break
     
     instruction_list.append(line.split())
     line_count += 1
 
 
+if(line_count>256) :
+    print("No of Instructions Exceeded")
 
+print(instruction_list)
