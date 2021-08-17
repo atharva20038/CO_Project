5851208a56c2b54abb425fdd6378a82c0da5791e
atharva20038 <atharva20038@iiitd.ac.in>
2021-08-17 18:03:28 +0530

    Added Type E and C functions with testing

--- SimpleSimulator/Simulator.py
+++ SimpleSimulator/Simulator.py
@@ -11,8 +11,9 @@
 
 def PRINT():
     pc_bin = bin(pc)[2:]
-    pc_bin = 0 * (8-len(pc_bin)) + pc_bin
-    print(pc_bin, registers['000'], registers['001'], registers['010'], registers['011'], registers['100'], registers['101'], registers['110'], 0*12 + registers['111']['V'] + registers['111']['L'] + registers['111']['G'] + registers['111']['E'])
+    #error correction
+    pc_bin = str(0 * (8-len(pc_bin))) + pc_bin
+    print(pc_bin, registers['000'], registers['001'], registers['010'], registers['011'], registers['100'], registers['101'], registers['110'], '0'*12 + registers['111']['V'] + registers['111']['L'] + registers['111']['G'] + registers['111']['E'])
 
 #Type - A
 
@@ -30,9 +31,9 @@ def add(code):
         registers[reg1] = bin(sum)[-16:]
     else:
         temp = bin(sum)[2:]
-        registers[reg1] = 0 * (16 - len(temp)) + temp 
+        registers[reg1] = '0' * (16 - len(temp)) + temp 
     
-    PRINT()
+  
 
     return 1  #increment of program counter
 
@@ -65,16 +66,64 @@ def ls(code):
 #Type - C
 
 def mov_reg(code):
-    pass
+    r1 = code[10:13]
+    r2 = code[13:16]
+
+    #moving r2 to r1 
+    registers[r1] = registers[r2]
+
+    
+
 
 def div(code):
-    pass
+    r3 = code[10:13]
+    r4 = code[13:16]
+
+    #storing quotient in '000' and remainder in '001'
+
+    #storing the quotient
+    r0 = bin(int(int(registers[r3],2)/int(registers[r4],2)))[2:]
+    registers['000'] = '0'*(16-len(r0)) + r0
+
+    #storing the remainder
+    r1 = bin(int(registers[r3],2) - int(registers['000'],2)*int(registers[r4],2))[2:]
+    registers['001'] = '0'*(16-len(r1)) + r1 
+    
+
 
 def inv(code):
-    pass
+    ###need confirmation on some part ....waiting for confirmation
+    r1 = code[10:13]
+    r2 = code[13:16]
+    
+    #we store the result in register-1 
+    temp = ''
+
+    for i in range(0,len(registers[r2])) :
+        if(registers[r2][i]=='0') : 
+            temp += '1'
+        
+        else :
+            temp += '0'
+
+
+    registers[r1] = '0'*(16-len(temp)) + temp
+        
+
 
 def cmp(code):
-    pass
+    r1 = code[10:13]
+    r2 = code[13:16]
+    value1 = int(registers[r1],2)
+    value2 = int(registers[r2],2)
+
+    if(value2<value1) : 
+        registers['111']['G'] = '1'
+    elif value2>value1 :
+        registers['111']['L'] = '1'
+    else : 
+        registers['111']['E'] = '1'
+    
 
 #Type - D
 
@@ -93,16 +142,27 @@ def st(code):
 #Type - E
 
 def jmp(code):
-    pass
+    address = code[8:16]
+    print(int(address,2))
+    return int(address,2)
 
 def jlt(code):
-    pass
+    if registers['L'] == '1' : 
+        return int(code[8:16],2)
+    else : 
+        return 1
 
 def jgt(code):
-    pass
+    if registers['G'] == '1' : 
+        return int(code[8:16],2)
+    else : 
+        return 1
 
 def je(code):
-    pass
+    if registers['E'] == '1' : 
+        return int(code[8:16],2)
+    else : 
+        return 1
 
 #Type - F
 
@@ -124,11 +184,21 @@ pc = 0
 
 #register values
 registers = {
-    '000': '0' * 16,
+    # '000': '0' * 16,
+    # '001': '0' * 16, 
+    # '010': '0' * 16,
+    # '011': '0' * 16,
+    # '100': '0' * 16,
+    # '101': '0' * 16,
+    # '110': '0' * 16,
+
+    ####TESTING.....
+
+    '000': '0000000000001111',
     '001': '0' * 16, 
     '010': '0' * 16,
-    '011': '0' * 16,
-    '100': '0' * 16,
+    '011': '0000000000001111',
+    '100': '0000000000000111',
     '101': '0' * 16,
     '110': '0' * 16,
     '111': {'V':'0', 'L': '0', 'G': '0', 'E': '0'}
@@ -139,14 +209,18 @@ registers = {
 #counter
 line_counter = 0
 
-while True:
-    try:
-        instruction = input().strip()
-        memory[line_counter] = instruction
-        line_counter += 1
+# while True:
+#     try:
+#         instruction = input().strip()
+#         memory[line_counter] = instruction
+#         line_counter += 1
     
-    except:
-        break
+#     except:
+#         break
+
+#####Testing......
+jmp('0111100000001111')
+PRINT()
 
 
 while pc<line_counter:
@@ -170,8 +244,9 @@ while pc<line_counter:
 
 #printing memory dump
 
-for x in memory:
-    print(x)
+#####testing.... so removed this bit temporarily
+# for x in memory:
+#     print(x)
 
 #bonus plot
 # plt.plot(cycle_list, pc_list)
